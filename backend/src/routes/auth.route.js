1f4818a3183c46eb1ce6caa8e7da787f2f253575
arikaran03 <arikaran258@gmail.com>
2025-05-27 21:24:54 +0530

    Add onboarded page backend work

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login, signup, logout } from '../controllers/auth.contoller.js';
+import { login, signup, logout, onboard } from '../controllers/auth.contoller.js';
+import productRoute from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post("/login", login);
 router.post("/signup", signup);
 
 router.post("/logout", logout);
+
+router.post("/onboarding", productRoute, onboard);
 
 export default router;
